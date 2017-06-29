733431183afcf51b87fc647682fecf08d2ec4b10
yucheng <a88426370@gmail.com>
2017-06-29 19:54:29 +0800

    Q488-Triangle Wave

---
+++ Q488.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void printtri(void);
+
+int n=0;
+
+int main(void){
+	while(scanf("%d",&n)!=EOF){
+		printtri();
+	}
+	/*scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		for(int j=0;j<=i;j++){
+			printf("%d",i+1);
+		}
+		printf("\n");
+	}
+	for(int i=1;i<n;i++){
+		for(int j=n-1;j>=i;j--){
+			printf("%d",n-i);
+		}
+		printf("\n");
+	}*/
+}
+void printtri(void){
+	for(int x=0;x<n;x++){//work n time
+		int A=0,F=0;
+		scanf("%d %d",&A,&F);
+		for(int y=0;y<F;y++){ //work F time
+			for(int i=0;i<A;i++){
+				for(int j=0;j<=i;j++){
+					printf("%d",i+1);
+				}
+				printf("\n");
+			}
+			for(int i=1;i<A;i++){
+				for(int j=A-1;j>=i;j--){
+					printf("%d",A-i);
+				}
+				printf("\n");
+			}
+			printf("\n");
+		}
+	}	
+}
